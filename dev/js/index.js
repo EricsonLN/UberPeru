5635ee425a4a3e6a6dd87e02016a6ef80d19d105
EricsonLN <lopezericson233@gmail.com>
2021-01-06 01:57:24 -0500

    Se agrego el header y menu responsive

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,1 +1,45 @@
-console.log(`Hola`);
+// normal
+const menuItem = document.getElementById('menuItem')
+// icono para q aparesca la x
+const showFlechaUp=document.getElementById('show-flecha-up')
+const showFlechaSown=document.getElementById('show-flecha-sown')
+
+// aparesca el submenu
+const subMenu=document.getElementById('submenu')
+
+if(menuItem){
+    const visibleProductos = document.getElementById('visibleProductos');
+    const visibleLenguage = document.getElementById('visibleLenguage');
+    menuItem.addEventListener('click',(e)=>{
+
+        subMenu.classList.toggle('show-submenu')
+        showFlechaUp.classList.toggle('main-flecha-up')
+        showFlechaSown.classList.toggle('fa-flecha-down')
+        visibleProductos.classList.toggle('topredirctbootoon')
+        visibleLenguage.classList.toggle('topredirctbootoon')
+        
+        
+    } )
+}
+const menubar = document.getElementById('menubar');
+const menuResponsiveShow = document.getElementById('menuResponsiveShow');
+const visibleProductos = document.getElementById('visibleProductos');
+const visibleLenguage = document.getElementById('visibleLenguage');
+const menuBurger = document.getElementById('menuBurger');
+const menuCerrar = document.getElementById('menuCerrar');
+// const menuCerrar = document.getElementById('menuCerrar');
+
+if(menubar){
+    menubar.addEventListener('click',(e)=>{
+
+        menuResponsiveShow.classList.toggle('main-nav__show')
+        visibleProductos.classList.toggle('productos_shows')
+        visibleLenguage.classList.toggle('productos_shows')
+        menuBurger.classList.toggle('menuBurger')
+        menuCerrar.classList.toggle('menucerrarshow')
+        // menuCerrar.classList.toggle('menuCerrar')
+        
+        
+        
+    } )
+}
