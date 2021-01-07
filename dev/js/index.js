87387949784556ad058fa66abc1014b077e3680b
EricsonLN <lopezericson233@gmail.com>
2021-01-07 14:34:01 -0500

    Se termino la web en la version de escritorio

--- dev/js/index.js
+++ dev/js/index.js
@@ -37,9 +37,58 @@ if(menubar){
         visibleLenguage.classList.toggle('productos_shows')
         menuBurger.classList.toggle('menuBurger')
         menuCerrar.classList.toggle('menucerrarshow')
-        // menuCerrar.classList.toggle('menuCerrar')
         
+    } )
+}
+const mainInfoCovid= document.getElementById('mainInfoCovid');
+const closeInfoCovid= document.getElementById('closeInfoCovid');
+if(closeInfoCovid){
+    closeInfoCovid.addEventListener('click',(e)=>{
+
+        mainInfoCovid.classList.toggle('close-info__covid')
         
         
     } )
 }
+
+
+$('#modeloCambiar img').hide();
+$('#modeloCambiar img:first').show();
+$("select[name=style]").change(function(){
+  estilo = $(this).val();
+  $('.dNone').removeAttr('selected'); 
+  $('.dNone').hide();
+  $('.'+estilo).show();
+  $('.'+estilo+':first').attr('selected','selected');
+  estilo = $('.'+estilo+':first').val();
+  $('#modeloCambiar img').hide();
+  $('.'+estilo).fadeToggle(1200);
+  
+});
+
+$("select[name=model]").change(function(){
+  estilo = $(this).val();
+  $('#modeloCambiar img').hide();
+  $('.'+estilo).fadeToggle(1200);
+});
+
+$("#modelo option").css("display","none");
+$('#vehiculo').on('change', function() {
+  $("#modelo option").css("display","none");
+  $("."+this.value+"").css("display", "block");
+  $("#modelo").prop('selectedIndex', 0);
+});
+
+
+var btn = $(".btn-habilitar");
+btn.attr('disabled', 'disabled');
+btn.css('background', 'rgb(34 34 34 / 0.3)');
+$('#txtterminos1').on('click', function() {
+  if ($(this).is(':checked')) {
+    btn.removeAttr('disabled');
+    btn.css('background', '#4184d2');
+  } else {
+    btn.attr('disabled', 'disabled');
+    btn.css('background', 'rgb(34 34 34 / 0.3)');
+  }
+});
